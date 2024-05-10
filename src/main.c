dbb40b88c6c4fdf813a9dd08e21f1c208daa9ab4
Caspar Gutsche <caspargutsche1@gmail.com>
2024-05-10 18:50:48 +0200

    2d array

--- src/main.c
+++ src/main.c
@@ -3,9 +3,17 @@
 #include <stdlib.h>
 #include <assert.h>
 
-// float f(float x, float y) {
-//     return (x * x + y * y)/2;
-// }
+
+#define L 100
+#define d 2
+#define N pow(L,d)
+// int L = 100;
+// int d = 2;
+// int N = pow(L,d);
+
+float f2(float x, float y) {
+    return (x * x + y * y)/2;
+}
 
 double f(double x) //why does this conflict with the other f function? They have different signatures?
 {
@@ -52,6 +60,24 @@ double* second_derivative(double* f, int size, double dx) {
     return ddf;
 }
 
+// double** laplace_2d(int size, double dx, double f[size][size]) {
+//     // double* (*laplace)[size] = malloc(sizeof(double[size][size]));
+//     // double* laplace = malloc(size*size*sizeof(double));
+//     double laplace[size][size];
+//     for (int i = 0; i < size; i++) {
+//         for (int j = 0; i < size; j++) {
+//             if (i == 0) {
+//                 // todo
+//             } else if (i == size - 1) {
+//                 // todo
+//             } else {
+//                 laplace[i][j] = (f[i + 1][j] - 2 * f[i][j] + f[i - 1][j]) / (dx * dx) + (f[i][j + 1] - 2 * f[i][j] + f[i][j - 1]) / (dx * dx);
+//             }
+//         }
+//     }
+//     return laplace;
+// }
+
 int test() {
     int i, j;
     int n = 1000;
@@ -74,7 +100,40 @@ int test() {
     return 0;
 }
 
+
+int get_index(int* cords) {
+    int i;
+    for (i=0; i<=d; i++) {
+        i += L*cords[i];
+    }
+    return i;
+}
+
+int* index_to_cords(int i) {
+    int* cords = malloc(N*sizeof(int));
+    for (i=0; i<L; i++)
+    {
+        cords[i] = i % L;
+        i /= L;
+    }
+    return cords;
+}
+
 int main() {
     test();
+
+    int i, j;
+    double dx = 2.0 / (L - 1);
+    double x, y;
+    double* u = (double*) malloc(N*sizeof(float));
+    for (i = 0; i < N; i++) {
+        // x,y = index_to_cords(i);
+        x = index_to_cords(i)[0];
+        y = index_to_cords(i)[1];
+        x = -1 + i * dx;
+        y = -1 + j * dx;
+        u[i] = f2(x, y);
+        printf("%f", u[i]);
+    }
     return 0;
 }
