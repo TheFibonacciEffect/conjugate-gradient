7feb676b1156b36ecd29000476dcc367037aef41
Caspar Gutsche <caspargutsche1@gmail.com>
2024-07-05 14:48:54 +0200

    WIP: implement gc precon gpu

--- src/main.cu
+++ src/main.cu
@@ -9,14 +9,11 @@
 // #define L 5  // Lattice size
 // #define N (int)pow(L,d) // Number of lattice points
 
-#define dmax 5  // Dimension
+#include "conjugate-gradient_cpu.h"
 
 #include "laplace-d.h"
-#define TYPE float
-// #include "laplace-x.h"
-
-#include "conjugate-gradient_cpu.h"
 #include "preconditioner_gpu.h"
+
 int main()
 {
     run_tests_cpu();
@@ -25,17 +22,9 @@ int main()
     int N = (int)pow(L,d);
     double* b = cuda_allocate_field_d(N);
     double* x = cuda_allocate_field_d(N);
-    double* x_cpu = (double*)malloc(N*sizeof(double));
-    apply_function_gpu_d<<<1000,128>>>(x,N,L,d);    
-    cudaDeviceSynchronize();
-    cudaMemcpy(x_cpu,x,N*sizeof(double),cudaMemcpyDeviceToHost);
-    // for (int i = 0; i < N; i++)
-    // {
-    //     printf("%f\n",x_cpu[i]);
-    // }
 
     // TODO Undefined reference
-    // preconditioner_gpu(b,x,L,d,1e-3);
+    preconditioner_gpu(b,x,L,d,1e-3);
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
 }
---
+++ src/preconditioner_gpu.h
@@ -0,0 +1,1 @@
+void preconditioner_gpu(double* b, double* x, int L, int d, double errtol);
