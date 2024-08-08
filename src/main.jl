6a11fbbb712b796f73b24f01ddfc5387db8dd098
Caspar Gutsche <caspargutsche1@gmail.com>
2024-08-08 22:27:12 +0200

    wrapper function

--- src/main.jl
+++ src/main.jl
@@ -35,6 +35,12 @@ function neighbour_index_gpu(ind, direction, amount, L, d, N, index_mode)::Cint
     @ccall $sym(ind::Cint, direction::Cint, amount::Cint, L::Cint, d::Cint, N::Cint, index_mode::Cint)::Cint
 end
 
+# Define the wrapper function for `conjugate_gradient_gpu`
+function conjugate_gradient_gpu(b::CuArray{Float32}, x::CuArray{Float32}, L::Cint, d::Cint)::Cfloat
+    sym = Libdl.dlsym(lib, :conjugate_gradient_gpu)
+    @ccall $sym(get_ptr(b)::CuPtr{Cfloat}, get_ptr(x)::CuPtr{Cfloat}, L::Cint, d::Cint)::Cfloat
+end
+
 # Example usage
 N = 10
 A = CUDA.fill(1.0f0, N)
